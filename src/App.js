d75060f0502cb674f17f66a3cace63be0ce75745
Vanessa Singh <vns.28singh@gmail.com>
2020-10-13 22:32:44 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ChoroplethMap from './components/ChoroplethMap';
+// <ChoroplethMap data={this.state.data}/>
+class App extends Component {
+  state = {
+    data: [
+      ["CA", 75], ["NY", 10]],
+  }
+  render() {
+    return (
+      <div className="App">
+
+        <div className="App-header">
+          <p>Faculty Hiring Analysis</p>
+        </div>
+
+        <div style={{height:"100vh", width: "100vw"}} className = "Horizontal-Widget">
+          <ChoroplethMap data={this.state.data}/>
+        </div>
+        <div style={{height:"100vh", width: "100vw"}} className = "Horizontal-Widget">
+          <ChoroplethMap data={this.state.data}/>
+        </div>
+
+        
+      </div>
+      
+    );
+  }
 }
 
 export default App;
