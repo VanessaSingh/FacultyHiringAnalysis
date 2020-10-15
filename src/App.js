3fa1e75005ea20ba9ed7f48f63668788789c2ad7
Vanessa Singh <vns.28singh@gmail.com>
2020-10-15 18:20:16 -0400

    Add Radial Chart without tooltip

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React, { Component } from 'react';
 import ChoroplethMap from './components/ChoroplethMap';
 import { Container, Nav } from "./styled-components";
+import RadialChart from './components/RadialChart';
 
 // <ChoroplethMap data={this.state.data}/>
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
     data: [
       ["CA", 75], ["NY", 10]],
   }
+
   render() {
     return (
       <Container className ="App">
@@ -39,23 +41,36 @@ class App extends Component {
           {/* Row 1 */}
           <Container className="row">
             <Container className="col-md-6 mb-4">
-              <Container className="card grid-card card-colour">
+              <Container className="card big-grid-card card-colour">
                 <Container>
                   <h4>
-                    Graph 2
+                    Professors who did BS/MS/Phd/PostDoc from a US College
                   </h4>
                 </Container>
 
                 <Container className="card-value pt-4">
-                  <span>
-                    Hello World!
-                  </span>
+                  <Container className = "radial-charts">
+                    <RadialChart outer_circle_completion={100} inner_circle_completion={10} 
+                      outer_circle_colour="green" inner_circle_colour="blue"
+                    />
+                    <RadialChart outer_circle_completion={100} inner_circle_completion={10} 
+                      outer_circle_colour="green" inner_circle_colour="blue"
+                    />
+                    <br/>
+                    <RadialChart outer_circle_completion={100} inner_circle_completion={10} 
+                      outer_circle_colour="green" inner_circle_colour="blue"
+                    />
+                    <RadialChart outer_circle_completion={100} inner_circle_completion={10} 
+                      outer_circle_colour="green" inner_circle_colour="blue"
+                    />
+                  </Container>
+                  
                 </Container>
               </Container>
             </Container>
 
             <Container className="col-md-6 mb-4">
-              <Container className="card grid-card card-colour">
+              <Container className="card big-grid-card card-colour">
                 <Container>
                   <h4>
                     Graph 3
---
+++ src/components/RadialChart.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+class RadialChart extends Component {
+    state = {}
+    componentDidMount() {
+        // For initial animation
+        setTimeout(() => {
+            this.setState({ set_outer_stroke_len: true , set_inner_stroke_len: true});
+        });
+    }
+    render() {
+        const { set_outer_stroke_len, set_inner_stroke_len } = this.state;
+        const {
+            className,
+            outer_circle_colour, 
+            inner_circle_colour, 
+            outer_circle_completion, 
+            inner_circle_completion
+        } = this.props;
+        const stroke_width = 10;
+        const size = 200;
+
+        const outer_radius = 80;
+        const outer_circumference = 2 * 3.14 * outer_radius;
+        
+        const inner_radius = 50;
+        
+        const inner_circumference = 2 * 3.14 * inner_radius;
+        
+        const outer_stroke_len = set_outer_stroke_len ? outer_circumference / 100 * outer_circle_completion : 0;
+        const inner_stroke_len = set_inner_stroke_len ? inner_circumference / 100 * inner_circle_completion : 0;
+        
+        return (
+                <div
+                        className={classNames('radial-chart', className, {
+                            'no-outer_circle_completion': outer_stroke_len === 0
+                        })}
+                >
+                    <svg viewBox="0 0 180 180" width={size} height={size}>
+                        <circle
+                            className="radial-chart-total"
+                            stroke={outer_circle_colour}
+                            strokeWidth={stroke_width}
+                            fill="none"
+                            cx="90"
+                            cy="90"
+                            r={outer_radius}
+                        />
+                        <circle
+                            className="radial-chart-outer_circle_completion"
+                            stroke={outer_circle_colour}
+                            strokeWidth={stroke_width}
+                            strokeDasharray={`${outer_stroke_len},${outer_circumference}`}
+                            strokeLinecap="round"
+                            fill="none"
+                            cx="90"
+                            cy="90"
+                            r={outer_radius}
+                        />
+                        <circle
+                            className="radial-chart-total"
+                            stroke={inner_circle_colour}
+                            strokeWidth={stroke_width}
+                            fill="none"
+                            cx="90"
+                            cy="90"
+                            r={inner_radius}
+                        />
+                        <circle
+                            className="radial-chart-outer_circle_completion"
+                            stroke={inner_circle_colour}
+                            strokeWidth={stroke_width}
+                            strokeDasharray={`${inner_stroke_len},${inner_circumference}`}
+                            strokeLinecap="round"
+                            fill="none"
+                            cx="90"
+                            cy="90"
+                            r={inner_radius}
+                        />
+                    </svg>
+                </div>
+                );
+            }
+}
+
+RadialChart.propTypes = {
+    className: PropTypes.string,
+    outer_circle_colour: PropTypes.string,
+    inner_circle_colour: PropTypes.string,
+};
+export default RadialChart;
